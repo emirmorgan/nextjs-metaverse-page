d7c3edaad54a238c38c93d7c84f65b57c3808b19
emirmorgan <aemirdnr@gmail.com>
2022-12-15 06:06:54 +0300

    Update: Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { navVariants } from "../utils/motion";
+
+const Navbar = () => {
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className="relative py-8 px-6 sm:px-16"
+    >
+      <div className="flex justify-between w-full 2xl:max-w-[1280px] mx-auto gap-8">
+        <Image
+          src="/assets/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+          className="object-contain cursor-pointer"
+        />
+        <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+          METAVERSE
+        </h2>
+        <Image
+          src="/assets/menu.svg"
+          alt="menu"
+          width={24}
+          height={24}
+          className="object-contain cursor-pointer"
+        />
+      </div>
+      <div className="absolute z-[-1] w-[50%] inset-0 gradient-01"></div>
+    </motion.nav>
+  );
+};
+
+export default Navbar;
