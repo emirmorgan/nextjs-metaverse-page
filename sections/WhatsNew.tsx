0eadceb5fac23f60983b83509f2a55198d577343
emirmorgan <aemirdnr@gmail.com>
2022-12-16 22:13:56 +0300

    Update: World Section

--- sections/WhatsNew.tsx
+++ sections/WhatsNew.tsx
@@ -2,6 +2,21 @@ import { motion } from "framer-motion";
 import { planetVariants, staggerContainer, fadeIn } from "../utils/motion";
 
 const WhatsNew = () => {
+  const newFeatures = [
+    {
+      imgUrl: "/assets/vrpano.svg",
+      title: "A new world",
+      subtitle:
+        "we have the latest update with new world for you to try never mind",
+    },
+    {
+      imgUrl: "/assets/headset.svg",
+      title: "More realistic",
+      subtitle:
+        "In the latest update, your eyes are narrow, making the world more realistic than ever",
+    },
+  ];
+
   return (
     <section className="sm:p-16 xs:p-8 px-6 py-12">
       <motion.div
@@ -27,6 +42,28 @@ const WhatsNew = () => {
           >
             What&apos;s new about Metaversus?
           </motion.h2>
+          <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+            {newFeatures.map((feature, index) => (
+              <div
+                key={index}
+                className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]"
+              >
+                <div className="flex justify-center items-center w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]">
+                  <img
+                    src={feature.imgUrl}
+                    alt="icon"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+                <h1 className="mt-[26px] font-bold text-[24px] leading-[30.24px] text-white">
+                  Title {feature.title}
+                </h1>
+                <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+                  {feature.subtitle}
+                </p>
+              </div>
+            ))}
+          </div>
         </motion.div>
         <motion.div
           variants={planetVariants("right")}
--- sections/World.tsx
+++ sections/World.tsx
@@ -1,5 +1,63 @@
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
 const World = () => {
-  return <section>World</section>;
+  return (
+    <section className="sm:p-16 xs:p-8 px-6 py-12">
+      <motion.div
+        variants={staggerContainer(0.2, 1)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className="2xl:max-w-[1280px] w-full mx-auto flex flex-col gap-8"
+      >
+        <motion.span
+          variants={staggerContainer(0.2, 1)}
+          className="font-normal text-center text-[14px] text-secondary-white"
+        >
+          | People on the World
+        </motion.span>
+        <motion.h2
+          variants={staggerContainer(0.2, 1)}
+          className="text-center text-white mt-[8px] font-bold md:text-[64px] text-[40px]"
+        >
+          Track friends around you and invite them to play together in the same
+          world
+        </motion.h2>
+        <motion.div
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          className="relative mt-[68px] flex w-full h-[550px]"
+        >
+          <img
+            src="/assets/map.png"
+            alt="map"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+            <img
+              src="/assets/people-01.png"
+              alt="people"
+              className="w-full h-full"
+            />
+          </div>
+          <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+            <img
+              src="/assets/people-02.png"
+              alt="people"
+              className="w-full h-full"
+            />
+          </div>
+          <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+            <img
+              src="/assets/people-03.png"
+              alt="people"
+              className="w-full h-full"
+            />
+          </div>
+        </motion.div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default World;
